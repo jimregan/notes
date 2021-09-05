a7d66bd445b453d64c5361eb0d35e9da5028296d
Jim O’Regan <jaoregan@tcd.ie>
2021-09-05 14:52:56 +0100

    Old branch (#62)

--- _drafts/corpuscrawler-irish.py
+++ _drafts/corpuscrawler-irish.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # Copyright 2021 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
-# Copyright 2021 Jim O'Regan
+# Copyright 2021 Phonetics and Speech Laboratory, Trinity College, Dublin
 #
 # Based on Corpus Crawler (utils.py):
 # Copyright 2017 Google Inc. All rights reserved.
@@ -20,7 +20,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 # Lint as: python3
 """Corpus Crawler Irish web text dataset."""
 
@@ -35,9 +34,6 @@ from html.entities import name2codepoint
 from email import message_from_string as Message
 from urllib.parse import urlparse
 from pathlib import Path
-from typing import Optional
-
-import pyarrow as pa
 
 import datasets
 
@@ -54,20 +50,16 @@ _SCRAPES = ["20191117", "20210810"]
 
 
 logger = datasets.utils.logging.get_logger(__name__)
-_DATA_URL = 'https://gist.githubusercontent.com/jimregan/66612f4ecb88ed96d41d43266e6d0872/raw/26bd05f11b4c1c31e33d36528ac53dea587be8ef/crawled-{}.txt'
+_DATA_URL = 'https://huggingface.co/datasets/phonlab-tcd/corpuscrawler-ga/raw/main/crawled-{}.txt'
 
-
-class CorpusCrawlerIrishConfig(datasets.BuilderConfig):
-    """BuilderConfig for CorpusCrawlerIrish."""
-
-    def __init__(self, **kwargs):
-        super(CorpusCrawlerIrishConfig, self).__init__(version=datasets.Version("2.1.0", ""), **kwargs)
 
 class CorpusCrawlerIrish(datasets.GeneratorBasedBuilder):
     """Corpus Crawler crawled text dataset."""
 
     BUILDER_CONFIGS = [
-        CorpusCrawlerIrishConfig(name=scrape) for scrape in _SCRAPES
+        datasets.BuilderConfig(name=f"{scrape}_{cfg}")
+            for scrape in _SCRAPES
+            for cfg in ["documents", "paragraphs"]
     ]
 
     def _info(self):
@@ -86,21 +78,18 @@ class CorpusCrawlerIrish(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
-        if not self.config.data_dir:
-            raise ValueError(f"Path to Corpus Crawler cache directory must be specified, but got data_dir={self.config.data_dir}")
-        cc_cache = self.config.data_dir
+        manual_dir = os.path.abspath(os.path.expanduser(dl_manager.manual_dir))
 
-        if not self.config.name:
-            raise ValueError(f"Scrape set must be specified, but got name={self.config.name}")
         scrape_set = self.config.name
-        dl_path = dl_manager.download(_DATA_URL.format(self.config.name))
+        sset = self.config.name.split('_')[0]
+        dl_path = dl_manager.download(_DATA_URL.format(sset))
 
         return [
             datasets.SplitGenerator(
                 name=datasets.Split.TRAIN,
                 gen_kwargs={
                     "name": scrape_set,
-                    "data_dir": cc_cache,
+                    "data_dir": manual_dir,
                     "data_file": dl_path,
                 })
         ]
@@ -108,6 +97,7 @@ class CorpusCrawlerIrish(datasets.GeneratorBasedBuilder):
     def _generate_examples(self, name, data_dir, data_file):
         """Generate examples from a Corpus Crawl cache."""
         logger.info("generating examples from = %s", name)
+        scfg = self.config.name.split('_')[1]
         links = _get_links(data_file)
         if not self.config.data_dir:
             self.config.data_dir = data_dir
@@ -117,11 +107,19 @@ class CorpusCrawlerIrish(datasets.GeneratorBasedBuilder):
 
         _id = 1
         for link in links:
+            if not link:
+                continue
             res = self._fetch_page(link, data_dir)
-            for para in res['text']:
+            if res is None:
+                raise Exception("Failed to read " + link + " from " + data_dir)
+            if scfg == "documents":
+                text = ["\n".join(res.get('text', []))]
+            else:
+                text = res.get('text', [])
+            for para in text:
                 example = {
                     "genre": res.get('genre', ''),
-                    "url": res['location'],
+                    "url": res.get('location', link),
                     "publication_date": res.get('publication-date', ''),
                     "video_url": res.get('video', ''),
                     "title": res.get('title', ''),
@@ -246,9 +244,14 @@ def fetch(cache_dir, url):
                 # already encoded as bytes
                 pass
             headers = Message(headers)
+            if not content:
+                raise Exception("empty content")
             return FetchResult(headers, content, url, filepath)
+        else:
+            raise Exception("splitting headers and content failed")
     except IOError:
         raise Exception("fetch() failed")
+
 
 def do_udhr(fetchresult):
     out = {}
@@ -613,6 +616,8 @@ def do_forasnagaeilge_ie(fetchresult):
 
 def _get_links(scrape):
     links = set()
+    if not os.path.exists(scrape):
+        raise Exception(f"File {scrape} does not exist")
     with open(scrape) as f:
         for url in f.readlines():
             links.add(url.rstrip())
