814f7047c3ff704dd1d035a5e5fcbcd85e42fc7c
Jim O'Regan <jaoregan@tcd.ie>
2021-09-11 09:09:10 +0100

    copy

---
+++ _drafts/vosk-ffmpeg-glob.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
+import os
+import subprocess
+import glob
+from pathlib import Path
+
+SetLogLevel(0)
+
+model = Model("model")
+rec = KaldiRecognizer(model, sample_rate)
+rec.SetWords(True)
+
+if sys.argv and sys.argv[0]:
+    dir = Path(sys.argv[1])
+else:
+    exit()
+
+if not dir.is_dir():
+    exit()
+
+for file in dir.glob('*.mp3'):
+    outfile = dir / f"{file.stem}.json"
+    process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+                                str(file),
+                                '-ar', 16000, '-ac', '1', '-f', 's16le', '-'],
+                                stdout=subprocess.PIPE)
+
+    while True:
+        data = process.stdout.read(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            print(rec.Result())
+        else:
+            print(rec.PartialResult())
+
+    with open(outfile, "w") as f:
+        f.write(rec.FinalResult())
