0dc93ea118c8e6252bf8c39e4d46111a55608f7f
Jim O'Regan <joregan@kth.se>
2022-11-11 19:45:28 +0100

    do things to add a media path

--- _drafts/vtt_to_eaf.py
+++ _drafts/vtt_to_eaf.py
@@ -21,16 +21,21 @@ def get_args():
     """)
     parser.add_argument('files', type=str, nargs='+', help='list of files to convert')
     parser.add_argument('--tier-name', help='Name of the speech tier (from the VTT captions)', type=str, default="speech")
+    parser.add_argument('--media-from-vtt', help='Media name is same as .vtt, with that extension removed', action='store_true', default=False)
     args = parser.parse_args()
 
     return args
 
 
-def convert_vtt_to_elan(filename, tiername="whisper"):
+def convert_vtt_to_elan(filename, tiername="whisper", mediafile=None):
     outfile = filename.replace(".vtt", ".eaf")
     eaf = Eaf()
     eaf.add_tier(tiername)
-    count = 1
+    if mediafile is not None:
+        if mediafile.endswith(".mp4"):
+            eaf.add_linked_file(f"/fake/path/{mediafile}", f"./{mediafile}", "video/mp4")
+        else:
+            eaf.add_linked_file(f"/fake/path/{mediafile}", f"./{mediafile}")
     for caption in webvtt.read(filename):
         start = int(caption.start_in_seconds * 1000)
         end = int(caption.end_in_seconds * 1000)
@@ -43,7 +48,11 @@ def main():
     args = get_args()
 
     for file in args.files:
-        convert_vtt_to_elan(file, args.tier_name)
+        if args.media_from_vtt:
+            media = file.replace(".vtt", "")
+            convert_vtt_to_elan(file, args.tier_name, media)
+        else:
+            convert_vtt_to_elan(file, args.tier_name)
 
 
 if __name__ == '__main__':
