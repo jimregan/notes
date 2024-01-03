46859ae8129836dd3c9c2a1c626aa593df27d7d5
Jim O'Regan <joregan@kth.se>
2024-01-03 21:30:58 +0000

    adapt

--- _drafts/export-tabs-simple.pl
+++ _drafts/export-tabs-simple.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 my $last_empty = 1;
-my $repo = '';
 my $desc = '';
 my $url = '';
 
@@ -9,17 +8,12 @@ while(<>) {
 	chomp;
 	if(/^$/) {
 		$last_empty = 1;
-		if($url ne '') {
-			print "[$repo]($url) --- $desc\n\n";
-		}
+		print "[$desc]($url)\n\n";
 		$repo = $desc = $url = '';
 	} else {
 		if($last_empty == 1) {
 			$last_empty = 0;
-			if(/^([^:]*): (.*)$/) {
-				$repo = $1;
-				$desc = $2;
-			}
+			$desc = $_;
 		} else {
 			$url = $_;
 		}
