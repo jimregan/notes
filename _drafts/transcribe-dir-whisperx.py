878e845f55d707326d0aa05c9968e829e7ba97f8
Jim O'Regan <joregan@kth.se>
2024-03-14 09:53:47 +0100

    add script

---
+++ _drafts/transcribe-dir-whisperx.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import argparse
+import whisperx
+import torch
+
+
+def get_args():
+    parser = argparse.ArgumentParser("Transcribe a directory of wav files")
+
+    parser.add_argument("wav_directory", type=Path)
+    parser.add_argument("tsv_directory", nargs="?", type=Path)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    indir = args.wav_directory
+    if args.tsv_directory:
+        outdir = args.tsv_directory
+    else:
+        outdir = indir
+
+    model = whisperx.load_model("medium.en", device=device)
+
+    for wav in indir.glob("*.wav"):
+        audio = whisperx.load_audio(str(wav))
+        output = model.transcribe(audio, language="en")
+        model_a, metadata = whisperx.load_align_model(language_code="en", device=device)
+        result = whisperx.align(output["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+        outfile = outdir / f"{wav.stem}.tsv"
+        with open(outfile, "w") as of:
+            if "word_segments" in result:
+                for res in result["word_segments"]:
+                    of.write(f'{res["start"]}\t{res["end"]}\t{res["word"]}\n')
+
+
+if __name__ == '__main__':
+        main()
